2f3f3d67ce53c01da5a0dbf7cc40c4237dbb5f2e
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-06-03 21:05:55 +0530

    side bar changes

--- src/components/Today.js
+++ src/components/Today.js
@@ -2,7 +2,7 @@ import React from 'react'
 import CloudBack from '../assets/images/cloud-background.png';
 import Clear from '../assets/images/Clear.png'
 export default function Today({ data }) {
-    const getImage = (type) =>{
+    const getImage = (type) => {
         switch (type) {
             case "Clear":
                 return Clear
@@ -10,6 +10,15 @@ export default function Today({ data }) {
                 break;
         }
     }
+    console.log(data);
+
+    const getTodayDate = (dt) => {
+        const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const day = ["Sun", "Mon", "Tue", "Thu", "Fri", "Sat"]
+        console.log('month[dt.getMonth()]', month[dt.getMonth()])
+        return `${month[dt.getMonth()]}, ${dt.getDay()} ${day[dt.getDay()]}`
+    }
+
     return (
         <div className='today-card'>
             <div className='icon-container'>
@@ -19,7 +28,16 @@ export default function Today({ data }) {
             <div className='center-container'>
                 <span className='temp font-col'>{data.deg}<sub>&deg;C</sub></span>
             </div>
-            <h2 className='center-container font-col'>{data.desc}</h2>
+            <h2 className='center-container font-col desc'>{data.desc}</h2>
+            <div className='center-container'>
+                <span className='today-date'><span>Today</span> <span>&#183;</span><span>{getTodayDate(data.date2)}</span></span>
+            </div>
+            <span className='center-container today-date'>
+                <span class="material-icons material-symbols-outlined">
+                    location_on
+                </span>
+                <span>{data.city}</span>
+            </span>
         </div>
     )
 }
